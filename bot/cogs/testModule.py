b301c8dca6db43e6fa6db7b8a82e8b72828d55bc
chatsumlin <chat.sumlin@banyanhills.com>
2019-06-25 22:55:51 -0400

    added gitpull command

--- bot/cogs/testModule.py
+++ bot/cogs/testModule.py
@@ -3,6 +3,7 @@ import discord
 import re
 from loguru import logger
 from .utils import checks # This will say error in most IDSs, Ignore it
+import git
 
 
 
@@ -46,7 +47,20 @@ class CommonSpam(commands.Cog):
             else:
                 await ctx.send("Please specify a *correct* setting! (on | off)")
 
-
+    @commands.command()
+    async def gitpull(self, ctx):
+        git_dir = "./"
+        try:
+            g = git.cmd.Git(git_dir)
+            g.pull()
+            embed = discord.Embed(title="Successfully pulled from repository", color=0x00df00)
+            await ctx.send(embed=embed)
+        except Exception as e:
+            errno, strerror = e.args
+            embed = discord.Embed(title="Command Error!",
+                                  description=f"Git Pull Error: {errno} - {strerror}",
+                                  color=0xff0007)
+            await ctx.send(embed=embed)
 
 
 def setup(bot):
