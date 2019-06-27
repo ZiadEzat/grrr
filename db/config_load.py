ef0bf4fcfe1f6c128cb18db19e7ed57964893862
Vladimir <48794941+1a11@users.noreply.github.com>
2019-06-28 02:04:11 +0400

    Update config_load.py

--- db/config_load.py
+++ db/config_load.py
@@ -1,5 +1,7 @@
 import json
-
+"""
+    config_load() - callable function for parsing configuration files
+"""
 def config_load():
     with open('data/config.json', 'r', encoding='utf-8') as doc:
         return json.load(doc)
