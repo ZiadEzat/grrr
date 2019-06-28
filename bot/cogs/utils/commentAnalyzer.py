2c467db7c3e236a941824699cb43a41854ce69d2
Potato <hcprice115@gmail.com>
2019-06-27 18:51:41 -0600

    Update commentAnalyzer.py

--- bot/cogs/utils/commentAnalyzer.py
+++ bot/cogs/utils/commentAnalyzer.py
@@ -18,7 +18,7 @@ class Predictor():
         return response
 
     def get_severe_toxicity(self, comment, language='en'):
-        languages = ['en']
+        languages = ['en', 'fr', 'es']
         if language not in languages:
             raise  Exception("Invalid Language for Severe toxicity report")
         data = {'comment': {'text': comment},
@@ -108,41 +108,3 @@ class Predictor():
         if r.status_code != 200:
             raise Exception("Error Making Your Request")
         return json.loads(r.text)
-
-# TODO: Threat, Spam, Identity Attack
-
-# class Predictor():
-#     def __init__(self):
-#         self.payload = {'key': ''}
-#         self.headers = {'Content-Type': 'application/json'}
-#
-#     def get_toxicity(self, comment, language='en'):
-#         languages = ['en', 'fr', 'es']
-#         if language not in languages:
-#             raise Exception("Invalid Language for toxicity report")
-#         data = {'comment': {'text': comment},
-#                      'languages': [language],
-#                      'requestedAttributes': {'TOXICITY': {}, 'SEVERE_TOXICITY': {}}}
-#         return self._send_request(data)
-#
-#     def get_severe_toxicity(self, comment, language='en'):
-#         languages = ['en', 'fr', 'es']
-#         if language not in languages:
-#             raise Exception("Invalid Language for toxicity report")
-#         data = {'comment': {'text': comment},
-#                      'languages': [language],
-#                      'requestedAttributes': {'SEVERE_TOXICITY': {}}}
-#         return self._send_request(data)
-#
-
-#
-
-#
-#     def _send_request(self, data):
-#         r = requests.post("https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze",
-#                         params=self.payload,
-#                         headers=self.headers,
-#                             json=data)
-#         if r.status_code != 200:
-#             raise Exception("Error Making Your Request")
-#         return json.loads(r.text)
